0b1c3b51dbd6eb98666e6b9a8cf96c8f12d5cd15
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-27 14:39:52 -0600

    create_user

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/users_test');
+// Let's try the connection to the databae instance users_test
+mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
 mongoose.connection
   .once('open', () => console.log('good to go'))
   .on('error', (error) => {
     console.warn('Warning', error);
   });
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next text!
+    done();
+  });
+})
