3134073a4a5d31294c6f67044c614d9ed4bfb599
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-28 13:57:15 -0600

    handling failed inserts

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -10,11 +10,21 @@ describe('Validating records', () => {
     assert(message === 'The name is required');
   });
 
-  it('a user name should have at least 2 character', () => {
+  it('a user name should have at least 3 character', () => {
     const user = new User({ name: 'as' });
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
     assert(message === 'Name must be longer that 2 characters')
+  });
+
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch(validationResult => {
+        const { message } = validationResult.errors.name;
+        assert(message === 'Name must be longer that 2 characters');
+        done();
+      }) 
   });
 
 })
