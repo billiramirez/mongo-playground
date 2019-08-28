99905a6570a2d581d28b5ab24659552424ff5365
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-28 11:14:07 -0600

    udpate test

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise; //This is required since mongoose is waiting 
 
 // Let's try the connection to the databae instance users_test
 before((done) => {  //before is executed once for all your test
-  mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
+  mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true, useFindAndModify: false });
   mongoose.connection
     .once('open', () => {done();})
     .on('error', (error) => {
