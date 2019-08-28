3bf6b2ca1753d392df95b608bdbfecc25d3b5598
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-28 13:07:38 -0600

    Update operators

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ const User = require('../src/user');
 describe('Updating users', () => {
   let joe;
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(()=> done())
   });
@@ -49,6 +49,19 @@ describe('Updating users', () => {
       User.findByIdAndUpdate(joe._id, {name: 'Alex'}),
       done
     )
+  });
+
+  /**
+   *   MONGO UPDATE OPERATORS - MORE COMPLEX 
+   */
+
+  it('A user can have their postcount incremented by 1', (done) =>{
+    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(()=> User.findOne({ name: 'Joe' }))
+      .then( user => {
+        assert(user.postCount === 1);
+        done();
+      })
   });
 
 
