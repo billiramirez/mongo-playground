e9f7bafad6605fc47071825dfb3f8f546e87a400
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-28 13:48:49 -0600

    Mongoose validate

---
+++ users/test/validation_test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === 'The name is required');
+  });
+
+  it('a user name should have at least 2 character', () => {
+
+  });
+
+})
